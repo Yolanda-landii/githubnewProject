c2ff805ada1276ec14b3aa612ef7437d53abb0b5
Yolanda <yolmabot021@student.wethinkcode.co.za>
2022-06-13 02:25:51 +0200

    first project

---
+++ FizzBuzz.py
@@ -0,0 +1,25 @@
+
+import numbers
+
+
+# def userInput():
+number= int(input("Enter any number of your choice: "))
+
+
+def Process(number):
+    # userInput()
+    # for number in range(100):
+    if number %3==0 and number%5==0:
+        print("FizzBuzz")
+     
+    elif number%3==0:
+        print("Fizz")
+    
+    elif number%5==0:
+        print("Buzz")
+      
+    else:
+        print(number)
+
+
+Process(number)
